cec3f9699c0ab98f2b95488162d95c6e707d1c83
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-08-05 19:23:00 +0200

    Tests directory added

---
+++ tests/tests_dcll/test_dcll_create.c
@@ -0,0 +1,14 @@
+/*
+** Duo Project C Library
+** DCLL_C_Library
+** File description:
+** Tests the dcll_create() function.
+*/
+
+#include <criterion/criterion.h>
+#include "dcll.h"
+
+Test(dcll_create, test_for_memory_allocation_true)
+{
+    return;
+}
