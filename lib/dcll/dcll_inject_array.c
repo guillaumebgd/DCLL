75d0314c19e325ba08bf563450a094d62c24179f
Guillaume Bogard <59202002+guillaumebgd@users.noreply.github.com>
2020-10-05 12:02:16 +0200

    Update dcll_inject_array.c

--- lib/dcll/dcll_inject_array.c
+++ lib/dcll/dcll_inject_array.c
@@ -10,11 +10,9 @@
 dcll_list_t *dcll_inject_array(dcll_list_t *list, const void *array,
                                 const size_t lenght, const size_t size)
 {
-    register size_t index = 0;
-
     if (!list || !array)
         return (NULL);
-    for (index = 0; index < lenght; index += 1) {
+    for (register size_t index = 0; index < lenght; index += 1) {
         dcll_add_node(list, (void *) array + (index * size), size, (void *)free);
     }
     return (list);
